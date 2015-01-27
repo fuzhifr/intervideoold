f13d8c1deecfbde6db14c971f1a110e2a6f6d07d
fuzhifr <fuzhifr@gmail.com>
2015-01-27 15:41:58 +0100

    deux fichier

---
+++ cacophony/server/php/StoryFile/2/YouTube.js
@@ -0,0 +1,4 @@
+_s[0] = [
+	{a:'bg_fade_in'}];
+_s[2]=[{a:'input_text_lp', d:{msg:"tirae",thanks:"Thanks for your input",
+		save_to: 'utils/save_input.php?questionName=tirae&userid=2&realname=YouTube',top:142.33333333333,left:120}},{a:'pause'}]; 
